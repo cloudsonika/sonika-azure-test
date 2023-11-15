172efddf36ab74476e402d94c078f17037977359
cloudsonika <113979766+cloudsonika@users.noreply.github.com>
2023-11-15 22:15:22 +0530

    Create newrelic.js

---
+++ newrelic.js
@@ -0,0 +1,11 @@
+// newrelic.js (or newrelic.cjs)
+
+'use strict';
+
+exports.config = {
+  app_name: ['Your Application Name'], // Set your application name here
+  license_key: 'YOUR_NEW_RELIC_LICENSE_KEY',
+  logging: {
+    level: 'info',
+  },
+};
